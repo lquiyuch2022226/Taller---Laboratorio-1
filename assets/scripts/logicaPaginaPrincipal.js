2851744cf0314520760c795f6b86a1c87538ffeb
lquiyuch2022226 <lquiyuch-2022226@kinal.edu.gt>
2024-01-25 23:59:16 -0600

    Se termina la página principal y se incluye canciones en la página

--- assets/scripts/logicaPaginaPrincipal.js
+++ assets/scripts/logicaPaginaPrincipal.js
@@ -1,5 +1,14 @@
-function reproducirMusica(enlace){
-    var cancion = enlace.getAttribute("data-cancion");
-    var audio = new Audio(cancion);
-    audio.play();
+var audio;
+
+function reproducirMusica(enlace) {
+    var cancion = enlace.getAttribute("ruta");
+    
+    if (!audio || audio.src !== cancion || audio.paused) {
+        if (audio) {
+            audio.pause();
+        }
+        
+        audio = new Audio(cancion);
+        audio.play();
+    }
 }
